f7a3a748154769c71756bc1c175ef884ac7c8453
Md Mostafa Niloy <mostafa@visernic.com>
2026-05-15 01:34:09 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,13 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // 1. Shudhu /webitpay te gele Rizon er home page asbe
       {
         source: '/webitpay',
-        destination: 'https://webitpay.vercel.app/',
+        destination: 'https://webitpay.vercel.app/webitpay',
       },
-      // 2. /webitpay/ er vitorer shobkichu (CSS, JS, Images, onnanno page) load hobe
       {
         source: '/webitpay/:path*',
-        destination: 'https://webitpay.vercel.app/:path*',
+        destination: 'https://webitpay.vercel.app/webitpay/:path*',
       },
     ];
   },

1453672db392e24d0a3bbb9e686bef944ee55795
Md Mostafa Niloy <mostafa@visernic.com>
2026-05-15 01:25:31 +0600

    Update redirect destinations in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,12 @@ const nextConfig = {
       // 1. Shudhu /webitpay te gele Rizon er home page asbe
       {
         source: '/webitpay',
-        destination: 'https://webitpay.vercel.app/webitpay',
+        destination: 'https://webitpay.vercel.app/',
       },
       // 2. /webitpay/ er vitorer shobkichu (CSS, JS, Images, onnanno page) load hobe
       {
         source: '/webitpay/:path*',
-        destination: 'https://webitpay.vercel.app/webitpay/:path*',
+        destination: 'https://webitpay.vercel.app/:path*',
       },
     ];
   },

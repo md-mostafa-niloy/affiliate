20c63d8e5969b0b7816e890b67038810c07b1ae7
Md Mostafa Niloy <mostafa@visernic.com>
2026-05-15 02:23:44 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/webitpay',
-        destination: 'https://webitpay.vercel.app/webitpay',
-      },
-      {
-        // Ekhane * er bodole + deya hoyeche jate CSS theek vabe load hoy
-        source: '/webitpay/:path+',
-        destination: 'https://webitpay.vercel.app/webitpay/:path+',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;

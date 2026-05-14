00e8caa806ab1bd31d288e3c588aa0f85a8ae62f
MD Mostafa Niloy <mostafa.niloys@gmail.com>
2026-05-15 01:09:51 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Jodi keu affiliate.visernic.com/webitpay te jay
+        source: '/webitpay',
+        destination: 'https://webitpay.vercel.app/webitpay',
+      },
+      {
+        // Jodi keu webitpay er vitorer kono page e jay (e.g. /webitpay/about)
+        source: '/webitpay/:path*',
+        destination: 'https://webitpay.vercel.app/webitpay/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
